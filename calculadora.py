669b22f8bb5ff3b7db599230083c8931b28f18e9
Raissa <raissa.info.cont@gmail.com>
2023-01-25 16:18:05 -0300

    Funcao de multiplicacao adicionada

--- calculadora.py
+++ calculadora.py
@@ -4,8 +4,13 @@ def soma(n1, n2):
 def subtracao(n1, n2):
     return n1 - n2
 
+def multiplicacao(n1, n2):
+    return n1 * n2
+
 resultado_soma = soma(1,4)
 resultado_subtracao = subtracao(1,4)
+resultado_multiplicacao = multiplicacao(1,4)
 
 print(f'Resultado da soma: {resultado_soma}')
 print(f'Resultado da subtracao: {resultado_subtracao}')
+print(f'Resultado da subtracao: {resultado_multiplicacao}')
--- soma.py
+++
@@ -1,5 +0,0 @@
-def soma(n1, n2):
-    return n1+n2
-
-resultado_soma = soma(1,4)
-print(f'Resultado da soma: {resultado_soma}')
